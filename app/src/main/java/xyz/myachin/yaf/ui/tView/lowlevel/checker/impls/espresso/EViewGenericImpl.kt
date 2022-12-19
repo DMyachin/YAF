1413f25915784b6c239d8201dcdd5c008a4af490
pzykov <zpavel411@gmail.com>
2022-12-19 15:13:30 +0300

    visible fix

--- app/src/main/java/xyz/myachin/yaf/ui/tView/lowlevel/checker/impls/espresso/EViewGenericImpl.kt
+++ app/src/main/java/xyz/myachin/yaf/ui/tView/lowlevel/checker/impls/espresso/EViewGenericImpl.kt
@@ -43,7 +43,12 @@ internal class EViewGenericImpl(private val eView: ViewInteraction) : TIViewGene
     }
 
     override fun isVisible(timeout: Long) {
-        isExists(timeout)
+//        isExists(timeout)
+        TWait.forViewInteraction(
+            eView,
+            ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)),
+            timeout
+        )
     }
 
     override fun isGone(timeout: Long) {
